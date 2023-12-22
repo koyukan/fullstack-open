e3042c073d267737de23f61a445c9b889f93e9cb
Kaan Koyukan <kaankoyukan@anayurtteknoloji.com>
2023-12-22 17:06:36 +0300

    Left@JSX

---
+++ part1/part1/src/Gallery.jsx
@@ -0,0 +1,15 @@
+import Profile from './Profile.jsx'
+
+const Gallery = () => {
+
+    return (
+      <section>
+        <h1>Amazing scientists</h1>
+        <Profile />
+        <Profile />
+        <Profile />
+      </section>
+    );
+  }
+
+  export default Gallery;
---
+++ part1/part1/src/Profile.jsx
@@ -0,0 +1,11 @@
+const Profile = () => {
+    return (
+      <img
+        src="https://i.imgur.com/MK3eW3Am.jpg"
+        alt="Katherine Johnson"
+      />
+    )
+  }
+
+
+export default Profile; 
